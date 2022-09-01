d7ce72b7fabd6de8364dc85529025f9d6d4ec32e
officialesse <uzomaesse@gmail.com>
2022-09-01 01:34:32 +0100

    feat: added nameable

--- person.rb
+++ person.rb
@@ -1,9 +1,17 @@
-class Person
+require_relative 'nameable'
+require_relative './decorators/capitalize_decorator'
+require_relative './decorators/trimmer_decorator'
+
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
+  @@num_instances = 0
+
   def initialize(age:, name: 'Unknown', parent_permission: true)
-    @id = Random.rand(1..1000)
+    super()
+    @@num_instances += 1
+    @id = @@num_instances
     @name = name
     @age = age
     @parent_permission = parent_permission
@@ -13,9 +21,20 @@ class Person
     of_age? || @parent_permission
   end
 
+  def correct_name
+    @name
+  end
+
   private
 
   def of_age?
     @age >= 18
   end
 end
+
+person = Person.new(age: 22, name: '   maximilianus   ')
+puts person.correct_name
+trimmed_person = TrimmerDecorator.new(person)
+puts trimmed_person.correct_name
+capitalized_trimmed_person = CapitalizeDecorator.new(trimmed_person)
+puts capitalized_trimmed_person.correct_name
