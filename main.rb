2bdbc26a5f4ee3a2a3ed0019650545ea45cfff7c
officialesse <uzomaesse@gmail.com>
2022-09-07 16:01:52 +0100

    feat:added refactored code

--- main.rb
+++ main.rb
@@ -1,8 +1,12 @@
 require_relative './app'
 
-def main
-  app = App.new
-  app.init
-end
-
-main
+def run_option(option, app)
+   case option when 1
+     then app.list_books when
+      2 then app.list_people when
+       3 then app.create_person when
+       4 then app.create_book when
+        5 then app.create_rental when
+         6 then app.list_rentals when
+         7 then app.quit_app else p
+         'Please enter a valid option, number from 1 to 7' end end
