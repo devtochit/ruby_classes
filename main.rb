ee28e47923cd3719477c96914d25354c2bb8c1f4
Brenda309 <brewihogora23@gmail.com>
2022-09-07 17:10:38 +0200

    remove case statements in the app.rb

--- main.rb
+++ main.rb
@@ -1,8 +1,15 @@
 require_relative './app'
+  def init
+    puts "Please choose an option by entering a number:
+        1 - List all books
+        2 - List all people
+        3 - Create a person
+        4 - Create a book
+        5 - Create a rental
+        6 - List all rentals for a given person ID
+        7 - Exit
 
-def main
-  app = App.new
-  app.init
-end
+      Enter your option here:"
+  end
 
-main
+ 
