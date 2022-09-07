4d9160e4820a61bf892d7fabb43e972af4f09fad
Brenda309 <brewihogora23@gmail.com>
2022-09-07 22:26:17 +0200

    Add file for data perservation

--- preserve_data.rb
+++ preserve_data.rb
@@ -3,3 +3,16 @@ require './student'
 require './book'
 require './rentals'
 
+module PreserveData
+
+    def save_rental(rental_list)
+        File.open('data/rentals.json', mode:'w')
+        rental_list.list.each do |data|
+            rental_person = [data.person.class,
+        data.person.age, data.person.name,
+        data.person.parent_permission]
+        rental_book = [data.book.title,data.book.author]
+        end
+    end
+    
+end
