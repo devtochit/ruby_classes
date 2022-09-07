d143e4a5231930b06ec51d1ac49aa7e603cefc1c
Brenda309 <brewihogora23@gmail.com>
2022-09-07 17:14:23 +0200

    add option to for case statements

--- main.rb
+++ main.rb
@@ -12,3 +12,41 @@ require_relative './app'
       Enter your option here:"
   end
 
+ def run_option(option, app)
+    case option
+    when 1 
+      then app.list_books
+    when 2 
+      then app.list_people
+    when 3 
+      then app.create_person
+    when 4 
+      then app.create_book
+    when 5 
+      then app.create_rental
+    when 6 
+      then app.list_rentals
+    when 7 
+      then app.quit_app
+    else p 'Please enter a valid option, number from 1 to 7'
+    end
+  end
+
+def main
+  app = App.new
+  option = 0
+  puts 'Welcome to School Library Rental App!'
+  while option != 7
+  init
+    option = gets.chomp.to_i
+    if option.positive? && option < 7
+      run_option(option, app)
+    elsif option == 7
+      puts 'Thanks for using this App!'
+    else
+      puts 'Invalid option'
+    end
+  end
+end
+
+main
