11af76141887bcaeb91603cf06ecf38252225bc9
Brenda309 <brewihogora23@gmail.com>
2022-09-08 23:16:35 +0200

    Add tests for student-classroom.

--- tests.rb
+++ tests.rb
@@ -4,3 +4,7 @@ require './student'
 require './classroom'
 require './book'
 
+p 'Student-Classroom tests'
+p pedro = Student.new(701, 12, 'Jesse Stud')
+p laura = Student.new(103, 21, 'Brenda student')
+p mairy = Student.new(207, 20, 'Cisco kelly')
