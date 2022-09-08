89afc979a749f6b76d369cc4329a366232b4221f
Brenda309 <brewihogora23@gmail.com>
2022-09-08 23:25:07 +0200

    Test add student.

--- tests.rb
+++ tests.rb
@@ -5,6 +5,15 @@ require './classroom'
 require './book'
 
 p 'Student-Classroom tests'
-p pedro = Student.new(701, 12, 'Jesse Stud')
-p laura = Student.new(103, 21, 'Brenda student')
-p mairy = Student.new(207, 20, 'Cisco kelly')
+p  Jesse = Student.new(701, 12, 'Jesse Stud')
+p Brenda = Student.new(103, 21, 'Brenda student')
+p  Cisco= Student.new(207, 20, 'Cisco kelly')
+
+p class619 = Classroom.new(101)
+p class619.students
+
+p jesse.classroom
+p class619.add_student(jesse)
+p jesse.classroom
+p jesse.classroom.label
+p class619.students
