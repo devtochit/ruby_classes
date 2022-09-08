c13bf51634707c87963684064289f808b2dcddf6
Brenda309 <brewihogora23@gmail.com>
2022-09-08 23:36:54 +0200

    Add student-classroom test case.

--- tests.rb
+++ tests.rb
@@ -8,3 +8,6 @@ p 'Student-Classroom tests'
 p jesse = Student.new(501, 19, 'Jesse stud')
 p kelly = Student.new(103, 21, 'Kelly stent')
 p mairy = Student.new(207, 20, 'Mairy Rilery')
+
+p class619 = Classroom.new(101)
+p class619.students
