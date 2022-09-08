634d520738139696ee0a3f99304fdf890fa1aff4
Brenda309 <brewihogora23@gmail.com>
2022-09-08 23:35:58 +0200

    Add tests for student-classroom.

--- tests.rb
+++ tests.rb
@@ -4,5 +4,7 @@ require './student'
 require './classroom'
 require './book'
 
-
-
+p 'Student-Classroom tests'
+p jesse = Student.new(501, 19, 'Jesse stud')
+p kelly = Student.new(103, 21, 'Kelly stent')
+p mairy = Student.new(207, 20, 'Mairy Rilery')
